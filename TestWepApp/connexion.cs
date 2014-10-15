7d7cf7af5fad84fbabc887091341fad9c2bbbe5f
ChrisCarlier <cj.carlier@gmail.com>
2014-10-15 10:13:02 +0200

    Update v4
    Possible de passer de la page de connexion a la page accueil_parent en
    tapant n' important quel identifiants et appuyer sur GO

---
+++ TestWepApp/connexion.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace TestWepApp
+{
+    public partial class connexion
+    {
+        SqlConnection connect;
+        SqlDataReader reader;
+        String connectionString;
+        SqlCommand cmd;
+
+
+    }
+}
